eb7099d0a09897f01c5854326b7cc762338abb73
Nithin S Varrier <nithin486@hotmail.com>
2023-06-18 20:58:10 +0530

    chore: update parser wheels, update logging method

--- src/lib/worker.ts
+++ src/lib/worker.ts
@@ -6,6 +6,30 @@ declare global {
   }
 }
 
+const { fetch: originalFetch } = self;
+
+self.fetch = async (...args) => {
+  const [resource, config] = args;
+  if (resource.toString().startsWith('emfs://')) {
+    const path = resource.toString().replace('emfs://', '');
+    const response = self.pyodide.FS.readFile(path);
+    return {
+      bodyUsed: false,
+      headers: {},
+      ok: true,
+      redirected: false,
+      status: 200,
+      statusText: 'OK',
+      type: 'basic',
+      url: resource,
+      arrayBuffer: () => response.buffer,
+    } as Response;
+  }
+
+  const response = await originalFetch(resource, config);
+  return response;
+};
+
 const FILE_IO = (() => {
   'use strict';
 
@@ -62,19 +86,23 @@ async function initialize() {
     fullStdLib: false,
   });
   await FILE_IO.initFS('/wheels');
+  console.log('Mount path');
   if (!self.pyodide.FS.analyzePath('/wheels/pdf_parser-1.0.0-py3-none-any.whl').exists) {
+    console.log('wheels does not exist');
     const zipPackage = await fetch('/wheels/wheels.zip');
     const zipPackageData = await zipPackage.arrayBuffer();
+    console.log('Fetched wheels');
     self.pyodide.unpackArchive(zipPackageData, 'zip', { extractDir: '/wheels' });
+    console.log('Unpacked wheels');
     await FILE_IO.closeFS();
   }
   await self.pyodide.loadPackage(['sqlite3', 'openssl', 'micropip']);
   const packages =
     '/wheels/cffi-1.15.1-cp311-cp311-emscripten_3_1_32_wasm32.whl,/wheels/chardet-5.1.0-py3-none-any.whl,/wheels/charset_normalizer-3.1.0-py3-none-any.whl,/wheels/click-8.1.3-py3-none-any.whl,/wheels/cryptography-39.0.2-cp311-cp311-emscripten_3_1_32_wasm32.whl,/wheels/dpath-2.1.6-py3-none-any.whl,/wheels/et_xmlfile-1.1.0-py3-none-any.whl,/wheels/numpy-1.24.2-cp311-cp311-emscripten_3_1_32_wasm32.whl,/wheels/opencv_python-4.7.0.72-cp311-cp311-emscripten_3_1_32_wasm32.whl,/wheels/openpyxl-3.1.2-py2.py3-none-any.whl,/wheels/pandas-1.5.3-cp311-cp311-emscripten_3_1_32_wasm32.whl,/wheels/pdfminer.six-20221105-py3-none-any.whl,/wheels/pycparser-2.21-py2.py3-none-any.whl,/wheels/PyMuPDF-1.22.3-cp311-cp311-emscripten_3_1_32_wasm32.whl,/wheels/pypdf-3.9.1-py3-none-any.whl,/wheels/python_dateutil-2.8.2-py2.py3-none-any.whl,/wheels/pytz-2023.3-py2.py3-none-any.whl,/wheels/six-1.16.0-py2.py3-none-any.whl,/wheels/tabulate-0.9.0-py3-none-any.whl,/wheels/camelot_fork-0.20.1-py3-none-any.whl,/wheels/pdf_parser-1.0.0-py3-none-any.whl';
-  const micropip = self.pyodide.pyimport('micropip');
   const packagePaths = packages.split(',').map((p) => `emfs://${p}`);
-  await micropip.install(packagePaths);
-  await self.pyodide.FS.unmount('/wheels');
+  for (const pkg of packagePaths) {
+    await self.pyodide.loadPackage(pkg, { messageCallback: (text) => self.postMessage({ result: text }) });
+  }
 }
 
 const initialized = initialize();
@@ -95,16 +123,17 @@ self.onmessage = async function (_e) {
   console.log('Pyodide is ready');
   const parser = self.pyodide.pyimport('pdf_parser');
   await self.pyodide.runPythonAsync(`
-    import logging
-    logger = logging.getLogger('pdf_parser')
-    camelotLogger = logging.getLogger('camelot')
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(formatter)
-    logger.addHandler(consoleHandler)
-    camelotLogger.addHandler(consoleHandler)
+  import logging
+  logger = logging.getLogger()
+  camelotLogger = logging.getLogger('camelot')
+  formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+  consoleHandler = logging.StreamHandler()
+  consoleHandler.setFormatter(formatter)
+  camelotLogger.handlers.clear()
+  logger.addHandler(consoleHandler)
+  logger.setLevel(logging.INFO)
 `);
-  const _parsed = parser.parse_pdf('/result.pdf', { pages: '3-4' });
+  const _parsed = parser.parse_pdf('/result.pdf');
   self.postMessage({ result: 'Exit', parsed: JSON.stringify(_parsed.toJs()) });
   alreadyRunning = false;
 };
